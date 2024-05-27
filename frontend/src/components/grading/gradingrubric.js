081409548c5dc55738badaa8700563ab1af5ab27
JiaaaJiaa <jiaying01@student.usm.my>
2024-05-28 03:15:11 +0800

    comment console.log

--- frontend/src/components/grading/gradingrubric.js
+++ frontend/src/components/grading/gradingrubric.js
@@ -7,7 +7,7 @@ function GradingRubric({assignment}) {
     const [totalMarks, setTotalMarks] = useState(0);
     useEffect(() => {
         if (assignment && assignment.assign_id) {
-            console.log('Assignment:', assignment.assign_id);
+            // console.log('Assignment:', assignment.assign_id);
             fetch(`/api/gradingrubric/${assignment.assign_id}`, {
                 method: 'GET',
                 headers: {
